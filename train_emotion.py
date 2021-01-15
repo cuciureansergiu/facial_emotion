944416d1c2958c1e44fec2337acaf8e495ba8923
Sergiu <sergiu.cuciurean@analog.com>
2021-01-16 00:31:37 +0200

    bak

--- train_emotion.py
+++ train_emotion.py
@@ -1,4 +1,5 @@
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 from sklearn import datasets, svm, metrics
 from matplotlib.pyplot import imread
 from fnmatch import fnmatch as match
@@ -8,7 +9,9 @@ from cv2 import resize
 import wget, os, re
 from hog import hog
 import numpy as np
+import progressbar
 import pickle
+import sys
 
 print("Training facial emotion model")
 
@@ -18,7 +21,7 @@ if (os.path.isdir('./KDEF_and_AKDEF') == False):
     filename = wget.download(url)
     with ZipFile('KDEF_and_AKDEF.zip', 'r') as zipObj:
         # Extract all the contents of zip file in different directory
-        zipObj.extractall('./KDEF_and_AKDEF')
+        zipObj.extractall('./')
 
 data_path = './KDEF_and_AKDEF/KDEF/'
 
@@ -32,28 +35,46 @@ dictionary = {
     "SU" : "surprised"
 }
 
+img_w = 512
+img_h = 512
+
 hog = hog()
 hog.summary()
 
+timecount = 0
+avg = 0
+# 140 actors * 7  emotions
+bar = progressbar.ProgressBar(maxval=140*7, \
+    widgets=[progressbar.Bar('=', '[', ']'),\
+    ' ', progressbar.ETA(),' ', progressbar.Percentage()])
+bar.start()
+
 labels = []
-data = np.zeros((1, int(hog.feature_size))) # Create empty image so the other can stack here
+data = np.zeros((1, int(hog.feature_size * 64))) # Create empty image so the other can stack here
 
 print("Reading the input data")
 for person in os.listdir(data_path):
     for image in os.listdir(data_path + person):
         # Only front facing profiles
         if match(image, '*S.JPG'):
+
             keyword = re.match(r"([a-z]+)([0-9]+)([a-z]+)", image, re.I)
             category = keyword.groups()[2]
             category = category[:-1]
-            labels.append(list(dictionary).index(category))
             img = imread(data_path + person + "/" + image)
-            img = resize(img, (64,64))
-            img = [hog.get_feature(rgb2gray(img))]
-            data = np.vstack((data,img))
-
-data = data[1:] # Delete the dummy image
+            img = resize(img, (img_w, img_h))
+            img = rgb2gray(img)
+            feature = []
+            for i in range(0, img_w, 64):
+                for j in range(0, img_h, 64):
+                    patch = img[i:i+64, j:j+64]
+                    feature = np.concatenate((feature, [hog.get_feature(patch)]), axis=None)
+            data = np.vstack((data,feature))
+            labels.append(list(dictionary).index(category))
+            bar.update(timecount := timecount + 1)
+data = data[1:] # Delete the dummy feature
 labels = np.array(labels)
+bar.finish()
 
 classifier = svm.SVC()
 data_train, data_test, labels_train, label_test = train_test_split(
